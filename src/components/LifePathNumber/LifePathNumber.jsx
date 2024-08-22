57e0bc16280d661d1aaec60eaad56a427114d8e4
Hazal <hazalsaygili96@gmail.com>
2024-08-21 20:19:14 -0400

    update lifePath calculation

--- src/components/LifePathNumber/LifePathNumber.jsx
+++ src/components/LifePathNumber/LifePathNumber.jsx
@@ -7,32 +7,109 @@ const LifePathNumber = ({ birthDay, birthMonth, birthYear }) => {
   const [lifePathObj, setLifePathObj] = useState(null);
 
   useEffect(() => {
-    const lifePathCalculator = (birthDay, birthMonth, birthYear) => {
-      const reduceToSingleDigit = (num) => {
-        while (num > 9 && ![11, 22, 33].includes(num)) {
-          num = num
-            .toString()
-            .split("")
-            .reduce((acc, digit) => acc + parseInt(digit), 0);
-        }
-        return num;
-      };
-
-      const reducedDay = reduceToSingleDigit(birthDay);
-      const reducedMonth = reduceToSingleDigit(birthMonth);
-      const reducedYear = reduceToSingleDigit(birthYear);
-
-      let birthdaySum = reducedDay + reducedMonth + reducedYear;
-      return reduceToSingleDigit(birthdaySum);
+    const lifePathCalculator = (day, month, year) => {
+      let birthDay = Number(day);
+      let birthMonth = Number(month);
+      let birthYear = Number(year);
+      let first_day_digit = 0;
+      let second_day_digit = 0;
+      let first_month_digit = 0;
+      let second_month_digit = 0;
+      let first_year_digit = Math.trunc(birthYear / 1000);
+      let second_year_digit = Math.trunc(birthYear / 100) - first_year_digit * 10;
+      let third_year_digit =
+        Math.trunc(birthYear / 10) -
+        second_year_digit * 10 -
+        first_year_digit * 100;
+  
+      let fourth_year_digit =
+        birthYear -
+        third_year_digit * 10 -
+        second_year_digit * 100 -
+        first_year_digit * 1000;
+      let lifePathNumber = 0;
+  
+      if (birthDay / 10 >= 1) {
+        first_day_digit = Math.trunc(birthDay / 10);
+        second_day_digit = birthDay % 10;
+      } else {
+        first_day_digit = birthDay;
+      }
+  
+      if (birthMonth / 10 >= 1) {
+        first_month_digit = Math.trunc(birthMonth / 10);
+        second_month_digit = birthMonth % 10;
+      } else {
+        first_month_digit = birthMonth;
+      }
+  
+      console.log(first_day_digit);
+      console.log(second_day_digit);
+      console.log(first_month_digit);
+      console.log(second_month_digit);
+  
+      let birthdaySum =
+        first_day_digit +
+        second_day_digit +
+        first_month_digit +
+        second_month_digit +
+        first_year_digit +
+        second_year_digit +
+        third_year_digit +
+        fourth_year_digit;
+  
+      console.log(birthdaySum);
+  
+      let first_birthdaySum_digit = 0;
+      let second_birthdaySum_digit = 0;
+  
+      if (birthdaySum === 11) {
+        return birthdaySum;
+      }
+  
+      if (birthdaySum === 22 || birthdaySum === 40) {
+        return 22;
+      }
+  
+      if (birthdaySum === 33) {
+        return birthdaySum;
+      }
+  
+      if (birthdaySum / 10 >= 1) {
+        first_birthdaySum_digit = Math.trunc(birthdaySum / 10);
+        second_birthdaySum_digit = birthdaySum % 10;
+        lifePathNumber = first_birthdaySum_digit + second_birthdaySum_digit;
+      } else {
+        lifePathNumber = birthdaySum;
+      }
+  
+      if (lifePathNumber === 11) {
+        return lifePathNumber;
+      }
+  
+      if (Math.trunc(lifePathNumber / 10) >= 1) {
+        lifePathNumber = Math.trunc(lifePathNumber / 10) + (lifePathNumber % 10);
+      }
+  
+      console.log(lifePathNumber);
+  
+      return lifePathNumber;
     };
-
-    let life_path_number = lifePathCalculator(birthDay, birthMonth, birthYear);
-    let newLifePathObj = lifePaths.find((obj) => obj.id == life_path_number);
+  
+    const life_path_number = lifePathCalculator(birthDay, birthMonth, birthYear);
+    console.log(lifePathNumber);
+    console.log(life_path_number);
+    const newLifePathObj = lifePaths.find((obj) => obj.id == life_path_number);
     setLifePathNumber(life_path_number);
     setLifePathObj(newLifePathObj);
   }, [birthDay, birthMonth, birthYear]);
 
-  if (!lifePathObj) return <p>Loading...</p>;
+
+  if (!lifePathObj) {
+  return <p>Loading...</p>;
+  }
+  
+  else
 
   return (
     <div className="numerology-section">
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,9 +5,17 @@ import "./NavBar.scss";
 const NavBar = () => {
   return (
     <nav className="navbar">
+      <div className="logo--container">
+      <img
+          src="/logo/crystalsilverlogo.png"
+          alt="crystalsilverlogo"
+          className="crystalsilver_logo"
+        />
       <NavLink to="/" className="logo">
-        COSMO APP
-      </NavLink>
+          COSMO APP 
+          {/* <span className="logo--span">by</span>  */}
+        </NavLink>
+      </div>
       <div className="nav-links">
         <NavLink to="/numerology" activeClassName="active">
           NUMEROLOGY REPORT
